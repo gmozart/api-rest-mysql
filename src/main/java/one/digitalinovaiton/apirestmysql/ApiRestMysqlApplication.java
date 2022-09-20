475062ac4a00f0f69146b760799453caefff3116
gmozart <mozart.gleison@gmail.com>
2022-09-19 21:08:48 -0300

    Incializando Api Rest

---
+++ src/main/java/one/digitalinovaiton/apirestmysql/ApiRestMysqlApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinovaiton.apirestmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestMysqlApplication.class, args);
+	}
+
+}
